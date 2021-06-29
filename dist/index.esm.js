0361ab94dc08dc2c35779da934798ad288a8ee45
virtualsamuraii <virtualsamurai@tutanota.com>
2021-06-29 02:04:15 +0200

    edit: nav icon size

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1874,7 +1874,7 @@ var Icon$P = function (props) {
 
 var Logo = function (_a) {
     var isDark = _a.isDark, props = __rest(_a, ["isDark"]);
-    return (React.createElement(Svg, __assign({ viewBox: "0 0 205 26" }, props),
+    return (React.createElement(Svg, __assign({ viewBox: "0 0 205 40" }, props),
         React.createElement("image", { width: "205", height: "40", href: isDark ? '/images/vpr/vaporwave_header_logo.png' : '/images/vpr/vaporwave_header_logo.png' })));
 };
 
